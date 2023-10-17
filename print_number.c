9c72b38ecc4bc9a05637c93a86b701225c64fbbb
missnike <adesinaadedamola4@gmail.com>
2023-10-17 23:50:49 +0000

    new number

--- print_number.c
+++ print_number.c
@@ -9,42 +9,35 @@ int print_number(int n)
 {
 	int j;
 	int i;
-    int count = 0;
-    char *min_int;
-    char n_arr[20];
+	int count = 0;
+	char *min_int;
+	char n_arr[20];
 
-    if (n == INT_MIN)
-    {
-        min_int = "-2147483648";
-        count += _puts(min_int);
-        return count;
-    }
-
-    if (n < 0)
-    {
-        count += _putchar('-');
-        n = -n;
-    }
-
-    if (n == 0)
-    {
-        count += _putchar('0');
-        return count;
-    }
-
-    
-    for (i = 0; n > 0; i++)
-    {
-        n_arr[i] = '0' + n % 10;
-        n /= 10;
-    }
-
-    count += i;
-
-    for (j = i - 1; j >= 0; j--)
-    {
-        _putchar(n_arr[j]);
-    }
-
-    return count;
+	if (n == INT_MIN)
+	{
+		min_int = "-2147483648";
+		count += _puts(min_int);
+		return (count);
+	}
+	if (n < 0)
+	{
+		count += _putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+	{
+		count += _putchar('0');
+		return (count);
+	}
+	for (i = 0; n > 0; i++)
+	{
+		n_arr[i] = '0' + n % 10;
+		n /= 10;
+	}
+	count += i;
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(n_arr[j]);
+	}
+	return (count);
 }
